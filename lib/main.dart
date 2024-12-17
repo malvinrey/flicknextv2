f04823606ab6233df30dde233520c7d05e052dec
johanesrendy <johanesrendy9@gmail.com>
2024-12-17 10:56:39 +0700

    integrating home_page detailfilm and watchlist page

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: '/',
       routes: {
-        '/': (context) => const HomePage(),
+        '/': (context) => HomePage(),
         '/login': (context) => const LoginPage(),
         '/register': (context) => SignUpScreen(),
         '/reset-password': (context) => ResetPasswordPage(),
